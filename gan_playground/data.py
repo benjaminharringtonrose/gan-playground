4ae21a098c130f5f0ae250cf85beb2541b077dc9
Benjamin Rose <55436674+benjaminharringtonrose@users.noreply.github.com>
2025-08-10 09:58:48 -0400

    saving progress

--- gan_playground/data.py
+++ gan_playground/data.py
@@ -11,4 +11,28 @@ def make_loader(batch_size=BATCH_SIZE):
         transforms.Normalize([0.5]*IMG_CH, [0.5]*IMG_CH),
     ])
     ds = datasets.MNIST(root="./data", train=True, download=True, transform=tfm)
-    return DataLoader(ds, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    return DataLoader(
+        ds, 
+        batch_size=batch_size, 
+        shuffle=True, 
+        num_workers=4, 
+        pin_memory=True,
+        persistent_workers=True
+    )
+
+def get_mnist_dataloader(batch_size=BATCH_SIZE, num_workers=4):
+    """Create MNIST data loader with preprocessing (alias for make_loader)"""
+    tfm = transforms.Compose([
+        transforms.Resize(IMG_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*IMG_CH, [0.5]*IMG_CH),
+    ])
+    ds = datasets.MNIST(root="./data", train=True, download=True, transform=tfm)
+    return DataLoader(
+        ds, 
+        batch_size=batch_size, 
+        shuffle=True, 
+        num_workers=num_workers, 
+        pin_memory=True,
+        persistent_workers=True
+    )
